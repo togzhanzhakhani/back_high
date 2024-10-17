9f5f716f6e91597669ff400373b405a0b4587449
togzhanzhakhani <t7zhan@gmail.com>
2024-10-17 09:08:50 +0500

    zaqaz setup

---
+++ zaqaz/shop/serializers.py
@@ -0,0 +1,12 @@
+from .models import Product, Order
+from rest_framework import serializers
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = '__all__'
